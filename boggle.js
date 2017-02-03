5682509fac022375313ecd1e5ad2c35deced3029
Diky Arga <dikyarga.id@gmail.com>
2017-02-03 11:23:01 +0700

    Semoga bener :D

--- boggle.js
+++ boggle.js
@@ -1,35 +1,65 @@
 class BoggleBoard {
   constructor(board) {
     this.board = board
-    this.word = ['A', 'P', 'E', 'L']
+    // Test correct
+    // this.word = ['A', 'P', 'E', 'L']
+    // Test another correcr
+    // this.word = ['B', 'L', 'K', 'Q']
+    // Test fail
+    this.word = ['B', 'L', 'K', 'G']
     this.letter = []
   }
 
-  findingNeighbors(i, j) {
-    console.log('' + this.board[i][j]);
+  findingNeighbors(i, j, next) {
+    // console.log('' + this.board[i][j]);
     let rowLimit = myArray.length-1;
     let columnLimit = myArray[0].length-1;
     for(let x = Math.max(0, i-1); x <= Math.min(i+1, rowLimit); x++) {
       for(let y = Math.max(0, j-1); y <= Math.min(j+1, columnLimit); y++) {
         if(x !== i || y !== j) {
-
-          console.log(this.board[x][y]);
+          // debugger
+          if (this.board[x][y] == next) {
+            return true
+          }
         }
       }
     }
+    return false
   }
 
   findChar(char){
     let rowLimit = myArray.length-1;
     let columnLimit = myArray[0].length-1;
-    let letter = {}
 
     for(let x = 0; x < rowLimit; x++) {
       for(let y = 0; y < columnLimit; y++) {
-        console.log(this.board[x][y]);
+        if (this.board[x][y] == char) {
+          // console.log('found ' + char + ' at ' + x + ' and ' + y);
+          return [x, y]
+        }
       }
     }
 
+  }
+
+  findChaining(){
+    let len = this.word.length -1
+    let foundCount = 0
+    for (let i = 0; i < len; i++) {
+      let charPos = this.findChar(this.word[i])
+      // console.log('next : ' + this.word[i+1]);
+      if (this.findingNeighbors(charPos[0], charPos[1], this.word[i+1])) {
+        foundCount++
+      }
+      // console.log('nextnya ketemu : ' + this.findingNeighbors(charPos[0], charPos[1], this.word[i+1]));
+    }
+    if (len == foundCount) {
+        console.log('Yes, bener, Sis!');
+        return true
+    } else {
+      console.log('Ah, sorry sis, gak ketemu!');
+      return false
+    }
   }
 
 }
@@ -39,7 +69,4 @@ let myArray = [['A', 'P', 'B', 'E'],
               ['G', 'T', 'Q', 'Z']]
 
 let mariMain = new BoggleBoard(myArray)
-
-mariMain.findingNeighbors(2, 1)
-// mariMain.findChar('E')
-// mariMain.findChar('L')
+mariMain.findChaining()
